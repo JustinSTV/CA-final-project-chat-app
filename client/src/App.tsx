7fc91feb118cabb43236a0cd85b8caf3952548d5
Justin S <steponaviciusjustas@gmail.com>
2024-10-28 11:21:16 +0200

    Logged in user cant click on himself, displaying other users

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import RegisterPage from './components/pages/RegisterPage';
 import MainPage from './components/pages/MainPage';
 import AllUsersPage from './components/pages/AllUsersPage';
 import UserProfile from './components/pages/UserProfile';
+import SpecificUserProfile from './components/pages/SpecificUserProfile';
 
 const App = () => {
 
@@ -21,6 +22,7 @@ const App = () => {
         <Route path='chat' element={<MainPage />}/>
         <Route path='users' element={<AllUsersPage />}/>
         <Route path='/profile/:username' element={<UserProfile />}/>
+        <Route path='/user/:username' element={<SpecificUserProfile />}/>
       </Route>
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
--- client/src/components/UI/molecule/UserCard.tsx
+++ client/src/components/UI/molecule/UserCard.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext, { UserContextTypes, UserType } from "../../../context/UserContext";
 
-import { UserType} from "../../../context/UserContext";
-
-const StyledDiv = styled.div`
+const StyledDiv = styled.div<{ isCurrentUser: boolean }>`
   background-color: #676765;
   border-radius: 10px;
   color: white;
@@ -12,6 +13,8 @@ const StyledDiv = styled.div`
   display: flex;
   align-items: center;
   gap: 20px;
+  cursor: ${({ isCurrentUser }) => (isCurrentUser ? "" : "pointer")};
+  opacity: ${({ isCurrentUser }) => (isCurrentUser ? 0.5 : 1)};
 
   >img{
     object-fit: cover;
@@ -22,9 +25,19 @@ const StyledDiv = styled.div`
 `
 
 const UserCard = ({user}: {user: UserType}) => {
+  const navigate = useNavigate();
+  const { loggedInUser } = useContext(UserContext) as UserContextTypes;
+
+  const isCurrentUser = loggedInUser?.username === user.username;
+
+  const handleClick = () => {
+    if (!isCurrentUser) {
+      navigate(`/user/${user.username}`);
+    }
+  };
 
   return (
-    <StyledDiv>
+    <StyledDiv onClick={handleClick} isCurrentUser={isCurrentUser}>
       <img src={user.profileImage} alt={`${user.username}`} />
       <p>{user.username}</p>
     </StyledDiv>
