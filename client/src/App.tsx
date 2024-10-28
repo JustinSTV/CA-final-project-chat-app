57ce4dea2a9119ede3e92670ca1940fd2e12f33b
Justin S <steponaviciusjustas@gmail.com>
2024-10-28 11:37:40 +0200

    little fixes regarding logged in user.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import UserContext, { UserContextTypes } from './context/UserContext';
 import LoginPage from './components/pages/LoginPage';
 import MainOutlet from './components/templates/MainOutlet';
 import RegisterPage from './components/pages/RegisterPage';
-import MainPage from './components/pages/MainPage';
 import AllUsersPage from './components/pages/AllUsersPage';
 import UserProfile from './components/pages/UserProfile';
 import SpecificUserProfile from './components/pages/SpecificUserProfile';
@@ -19,7 +18,6 @@ const App = () => {
       <Route path='login' element={<LoginPage />}/>
       <Route path='register' element={<RegisterPage />}/>
       <Route path="/" element={loggedInUser ? <MainOutlet /> : <Navigate to='/login' />}>
-        <Route path='chat' element={<MainPage />}/>
         <Route path='users' element={<AllUsersPage />}/>
         <Route path='/profile/:username' element={<UserProfile />}/>
         <Route path='/user/:username' element={<SpecificUserProfile />}/>
--- client/src/components/UI/molecule/UserCard.tsx
+++ client/src/components/UI/molecule/UserCard.tsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import UserContext, { UserContextTypes, UserType } from "../../../context/UserContext";
 
-const StyledDiv = styled.div<{ isCurrentUser: boolean }>`
+const StyledDiv = styled.div`
   background-color: #676765;
   border-radius: 10px;
   color: white;
@@ -13,16 +13,18 @@ const StyledDiv = styled.div<{ isCurrentUser: boolean }>`
   display: flex;
   align-items: center;
   gap: 20px;
-  cursor: ${({ isCurrentUser }) => (isCurrentUser ? "" : "pointer")};
-  opacity: ${({ isCurrentUser }) => (isCurrentUser ? 0.5 : 1)};
 
-  >img{
+  > img {
     object-fit: cover;
     border-radius: 50%;
     width: 50px;
     height: 50px;
   }
-`
+
+  &.disabled{
+    opacity: 0.5;
+  }
+`;
 
 const UserCard = ({user}: {user: UserType}) => {
   const navigate = useNavigate();
@@ -37,7 +39,7 @@ const UserCard = ({user}: {user: UserType}) => {
   };
 
   return (
-    <StyledDiv onClick={handleClick} isCurrentUser={isCurrentUser}>
+    <StyledDiv onClick={handleClick} className={isCurrentUser ? "disabled" : ""}>
       <img src={user.profileImage} alt={`${user.username}`} />
       <p>{user.username}</p>
     </StyledDiv>
